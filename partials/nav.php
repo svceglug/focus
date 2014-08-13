fecff3c088a3a3bd91d339b5a922e56c9e4af4d4
Nikhil Damle <nikhilbcd@gmail.com>
2014-08-13 14:55:45 +0530

    Add site header and nav bar

---
+++ partials/nav.php
@@ -0,0 +1,13 @@
+<header>
+  <div class="container">
+    <h1 class="site-title">FOCUS GLUG</h1>
+    <nav class="pull-right">
+      <ul>
+        <li><a href="/about.php">About</a></li>
+        <li><a href="/events.php">Events</a></li>
+        <li><a href="https://github.com/svceglug/">Git Repo</a></li>
+        <li><a href="/members.php">Members</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
