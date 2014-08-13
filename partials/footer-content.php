6f5bf0246c98ff98cf73652ad2e4a4a096a00e6e
Nikhil Damle <nikhilbcd@gmail.com>
2014-08-13 22:49:28 +0530

    Remove other glug links in footer

--- partials/footer-content.php
+++ partials/footer-content.php
@@ -20,18 +20,6 @@
       </div>
       <div class="column-1-4">
         <div class="footer-div">
-          <h2 class="gray-border">Other GLUG</h2>
-          <ul>
-            <li><a href="http://pluginatpes.wordpress.com/">PLUGIN</a></li>
-            <li><a href="http://draitglug.wordpress.com/">Dr. AIT GLUG</a></li>
-            <li><a href="http://glugpesce.blogspot.in/">PESCE GLUG</a></li>
-            <li><a href="http://gluegechassan.wordpress.com/">GLUE</a></li>
-            <li><a href="http://paceglug.blogspot.in/">PACE GLUG</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="column-1-4">
-        <div class="footer-div">
           <h2 class="gray-border">Creative Commons</h2>
         </div>
       </div>
