68f10e510543b666e417fb3395e26db45cdf9fc0
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-08-14 00:12:46 +0530

    members page

---
+++ members.php
@@ -0,0 +1,40 @@
+<?php require_once("partials/header.php"); ?>
+
+
+				<ul id="og-grid" class="og-grid">
+					<li>
+						<a href="https://www.facebook.com/bhargav.panth/" data-largesrc="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xpf1/t1.0-9/10393750_10203343314375686_4281107530156550109_n.jpg" data-title="Bhargav Panth" data-description="Bhargav is a great UI/UX designer, he is a great PHP programmer too. Bhargav is one of the initiators for forming FOCUS">
+							<img class="mem" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpf1/t1.0-1/p160x160/10393750_10203343314375686_4281107530156550109_n.jpg" alt="img01"/>
+						</a>
+					</li><li>
+						<a href="https://www.facebook.com/jinal.foflia" data-largesrc="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xfp1/t31.0-8/p417x417/10257908_10203644550175969_7441779222391519946_o.jpg" data-title="Veggies sunt bona vobis" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
+							<img class="mem" src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xfp1/t31.0-8/p417x417/10257908_10203644550175969_7441779222391519946_o.jpg" alt="img02"/>
+						</a>
+					</li><li>
+						<a href="https://github.com/nikhildamle" data-largesrc="https://scontent-b-sin.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10513373_551590254963536_2578667733481156161_n.jpg?oh=d60843f83b2acec32bf270919dfea499&oe=546484C3" data-title="Dandelion horseradish" data-description="Cabbage bamboo shoot broccoli rabe chickpea chard sea lettuce lettuce ricebean artichoke earthnut pea aubergine okra brussels sprout avocado tomato.">
+							<img class="mem" src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p160x160/10513373_551590254963536_2578667733481156161_n.jpg?oh=b1a2afcad8a20f07529d336332a8785e&oe=5462A882&__gda__=1417405831_5d93d5ef88bd531cdc6b98abedfc626c" alt="img03"/>
+						</a>
+					</li><li>
+						<a href="http://nishgtm.com" data-largesrc="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/423625_344753605568415_33449133_n.jpg" data-title="Nishchal Gautam" data-description="">
+							<img class="mem" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/t1.0-9/p417x417/423625_344753605568415_33449133_n.jpg" alt="img03"/>
+						</a>
+					</li>
+
+				</ul>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="js/grid.js"></script>
+		<script>
+			$(function() {
+				Grid.init();
+			});
+		</script>
+
+
+
+
+
+
+
+
+<?php require_once("partials/footer.php"); ?>
