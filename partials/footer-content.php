bd24dd453fa9b7d6b5a641cf260248a34c1923f4
Nikhil Damle <nikhilbcd@gmail.com>
2014-08-13 15:34:52 +0530

    Add footer content

---
+++ partials/footer-content.php
@@ -0,0 +1,40 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="column-1-4">
+        <div class="footer-div">
+          <h2 class="orange-border">FOCUS</h2>
+          <ul>
+            <li><a href="/contact.php">Contact Us</a></li>
+            <li><a href="https://github.com/scveglug">Source Code</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="column-1-4">
+        <div class="footer-div">
+          <h2 class="gray-border">Free Software</h2>
+          <ul>
+            <li><a href="http://www.fsmk.org/">Free Software Movement Karnataka</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="column-1-4">
+        <div class="footer-div">
+          <h2 class="gray-border">Other GLUG</h2>
+          <ul>
+            <li><a href="http://pluginatpes.wordpress.com/">PLUGIN</a></li>
+            <li><a href="http://draitglug.wordpress.com/">Dr. AIT GLUG</a></li>
+            <li><a href="http://glugpesce.blogspot.in/">PESCE GLUG</a></li>
+            <li><a href="http://gluegechassan.wordpress.com/">GLUE</a></li>
+            <li><a href="http://paceglug.blogspot.in/">PACE GLUG</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="column-1-4">
+        <div class="footer-div">
+          <h2 class="gray-border">Creative Commons</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
