e28881549d1465eab4b1923da60e4d5876532a20
Bhargav Panth <bhargavrpanth@gmail.com>
2014-08-14 23:56:52 +0530

    Added the info about our GLUG

---
+++ about.php
@@ -0,0 +1,48 @@
+
+<?php require 'partials/header.php' ?>
+
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/grids.css" />
+<!--    <link rel="stylesheet" href="css/application.css" />-->
+    <link rel="stylesheet" href='http://fonts.googleapis.com/css?family=Open+Sans:400,800'>
+  </head>
+  <body>
+    <div class="container">
+            <div class="row">
+                <h1 class="heading">About Us</h1>  
+                <div class="column-3-4"><h2> Free Software, Free Society</h2>
+                    <br>
+                <p align="justify">Free Software Movement Karnataka (FSMK) is a non-profit organization working for spreading free software and its ideals. The movement is inspired by software freedom visionaries like Richard Stallman.FSMK ia driven by volunteers from various background like software professionals, government officials, academicians and students.
+                In order to build a mass movement for free software, FSMK organizes GNU/Linux User's Groups (GLUG) in Engineering colleges which conducts various technical sessions throughout the academic session.</p>
+                </div>
+            <div class="column-1-4"><center><img src="img/eat.png"> </center> </div>
+         </div>
+        
+        <br>
+        
+        <div class="row">
+            <div class="column-1-4"><br><br><center><img src="img/sleep.png"></center></div>
+            <div class="column-3-4"><h2>FOCUS-GLUG</h2>
+                <br>
+            <p align="justify">&nbsp;&nbsp;FOCUS- Free software and Open SourCe Users-is started as a part of the FSMK by the students of Sri Venkateshwara College of Engineering. We too conduct free session through the academic year and propagate the message of 'Learning and Sharing' amongst us.
+</p>
+            </div>
+        </div>
+        
+        <br>
+        
+        <div class="row">
+            <div class="column-3-4"><h2>freedom software, Free as in Freedom</h2>
+                <br>
+                <p align="justify">"Free Software" means software that respects users' freedom and community, meaning it allows the user to share, copy, use, study, improve and use the softwares without any restrictions (copy right, patents, etc). Thus , "free software" is a matter of liberty and not price. We give an opportunity to be a part of this community, spread the joy of sharing and learning. </p>
+            </div>
+            <div class="column-1-4"><br><br><center><img src="img/code.png"></center></div>
+            </div>
+    </div>
+      <br>
+<!--
+  </body>
+</html>
+-->
+
+<?php require 'partials/footer.php' ?>
